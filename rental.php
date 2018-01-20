f88797f0574bce77b46757e8b23faacdddbaad1b
Alireza <etzous@gmail.com>
2018-01-20 23:24:08 +0000

    lab_3

--- rental.php
+++ rental.php
@@ -1,4 +1,28 @@
-<?php ?>
+<?php
+// db_connection.php
+
+    // connect to database
+    define("DB_SERVER", "localhost")      ;
+    define("DB_USER", "root")        ;
+    define("DB_PASS", "adminalizz")       ;
+    define("DB_NAME", "sakila")    ;
+
+    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+    // test connection
+    if(mysqli_connect_errno()) {
+        die("Database connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
+    }
+
+    $query  = "SELECT c.name AS genre, COUNT(fc.film_id) AS ";
+    $query .= "number_of_films FROM category c JOIN film_category fc ";
+    $query .= "ON c.category_id = fc.category_id GROUP BY fc.category_id";
+
+    $table_data = mysqli_query($connection, $query);
+    if (!$table_data) {
+        die("Database query failed!");
+    }
+?>
 
 <!DOCTYPE HTML>
 <html lang="en">
@@ -20,20 +44,22 @@
     </header>
     <div id="navigation">
         <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="rental.html">DVD RENTAL</a></li>
+            <li><a href="index.html">HOME</a></li>
+            <li><a href="rental.php">DVD RENTAL</a></li>
             <li><a href="return.html">DVD RETURN</a></li>
         </ul>
     </div>
     <div id="main">
-        <h2>RENT A DVD</h2>
-        <table>
-            <tr>
-            <th>Genres</th>
-                <th>Total number of available films</th>
-            </tr>
-        </table>
-        <div id="background">    
+        <div id="content">
+            <h2>RENT A DVD</h2>
+            <table>
+                <tr>
+                    <th>Genres</th>
+                    <th>Total number of available films</th>
+                </tr>
+            </table>
+        </div>
+        <div id="background">
         </div>
     </div>
 </body>
