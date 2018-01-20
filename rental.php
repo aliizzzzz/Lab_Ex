a8ae9d5cb25bccdd7750cbc8809440bed9d4d1e1
aliizzzzz <etzous@gmail.com>
2018-01-20 21:59:41 +0000

    Initial upload

---
+++ rental.php
@@ -0,0 +1,41 @@
+<?php ?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial scale=1">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+
+    <title>&num;1 Movies</title>
+</head>
+
+<body>
+    <header>
+        <h1><a href="index.html">&num;1 Movies</a></h1>
+    </header>
+    <div id="navigation">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="rental.html">DVD RENTAL</a></li>
+            <li><a href="return.html">DVD RETURN</a></li>
+        </ul>
+    </div>
+    <div id="main">
+        <h2>RENT A DVD</h2>
+        <table>
+            <tr>
+            <th>Genres</th>
+                <th>Total number of available films</th>
+            </tr>
+        </table>
+        <div id="background">    
+        </div>
+    </div>
+</body>
+
+</html>
